4b786d8bd09961d17dc948b3969a25a6bfaa7c7c
AthertonRoamer <111521556+AthertonRoamer@users.noreply.github.com>
2023-04-08 17:56:34 -0500

    small modifications

--- NetTest/data/lobby.gd
+++ NetTest/data/lobby.gd
@@ -2,13 +2,29 @@ extends Node
 
 var my_info := {}
 
+func start_server():
+	var peer = ENetMultiplayerPeer.new()
+	var err = peer.create_server(Data.server_port, Data.max_players)
+	print("err: " + str(err))
+	get_tree().get_multiplayer().multiplayer_peer = peer
+#	get_tree().set_multiplayer(peer)
+	start_lobby()
+	
+func start_client():
+	var peer = ENetMultiplayerPeer.new()
+	var err = peer.create_client(Data.ip, Data.server_port)
+	print("err: " + str(err))
+	get_tree().get_multiplayer().multiplayer_peer = peer
+#	get_tree().set_multiplayer(peer)
+
 func start_lobby():
 	var mult = get_tree().get_multiplayer()
 	mult.peer_connected.connect(_player_connected)
 	mult.peer_disconnected.connect(_player_disconnected)
 	
 func _player_connected(id : int):
-	Game.add_player(id)
+	Game.world.get_node("Waiting").visible = false
+	print("recieved connection!")
 	
 func _player_disconnected(id : int):
 	pass
--- NetTest/data/world.gd
+++ NetTest/data/world.gd
@@ -5,23 +5,14 @@ var missile_scene := preload("res://missile.tscn")
 func _ready():
 	Game.world = self
 	if Data.is_server:
-		begin_game()
+		Lobby.start_server()
 	else:
-		join_game()
+		Lobby.start_client()
 
 
-func begin_game():
-	var peer = ENetMultiplayerPeer.new()
-	var _err = peer.create_server(Data.server_port, Data.max_players)
-	get_tree().get_multiplayer().multiplayer_peer = peer
-#	get_tree().set_multiplayer(peer)
-	Lobby.start_lobby()
+
 	
-func join_game():
-	var peer = ENetMultiplayerPeer.new()
-	var _err = peer.create_client(Data.ip, Data.server_port)
-	get_tree().get_multiplayer().multiplayer_peer = peer
-#	get_tree().set_multiplayer(peer)
+
 	
 func add_bullet(pos : Vector2, source : int, dir : Vector2):
 	var m := missile_scene.instantiate()
